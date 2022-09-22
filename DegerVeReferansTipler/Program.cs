9732dc7d127bbd5458d88e75ec79a67909e7cea7
Yunus Emre Geyik <ynus.emr@hotmail.com>
2022-09-22 19:49:55 +0300

    C# Camp Day 3

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -19,8 +19,11 @@ namespace DegerVeReferansTipler
             int[] sayilar1 = new int[] {10,20,30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
-            sayilar2[0] = 999;
+            sayilar2[0] = 9999;
             Console.WriteLine(sayilar1[0]);
+
+
+
 
 
         }
